b8f5817cf1b787559888d422835cdb18c5a6a309
victor881212 <victor881212@gmail.com>
2023-02-23 13:50:43 +0800

    add homework1

---
+++ 作業/integrate.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+double integrate(double (*f)(double),float n1,float n2) {
+    double step = 0.001, sum=0.0;
+    for (double x=n1; x<n2; x+=step){
+        sum += f(x)*step;//x^2*step
+    }
+    return sum;
+// ...
+}
+
+double square(double x) {
+    return x*x;
+}
+
+int main() {
+    printf("integrate(square, 0.0, 2.0)=%f\n", integrate(square, 0.0, 2.0));  // 3/1x^3
+}
